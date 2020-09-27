20c6e185074e0fb3c797fe9c9ead472ccd50d210
Josh Seltzer <jselt88@gmail.com>
2020-09-27 04:28:25 -0400

    base code complete
    works; needs upgrades (color, comments, checks, other tbd upgrades)

--- js/script.js
+++ js/script.js
@@ -1,49 +1,61 @@
 /******************************************
-Treehouse FSJS Techdegree:
-project 1 - A Random Quote Generator
-******************************************/
+ Treehouse FSJS Techdegree:
+ project 1 - A Random Quote Generator
+ ******************************************/
 
 // For assistance:
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+// Check the "Project Resources" section of the project instructions
+// Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
-/*** 
- * `quotes` array 
-***/
+/***
+ * `quotes` array
+ ***/
 const quotes = [];
 
 function addQuote(quote, source, citation, year) {
-    if ((citation === undefined && year === undefined)) {
+    if (!quote || !source) {
+        console.log(`${quote} ${source} 'Needs a quote and source.'`);
+    } else if (!citation && !year) {
         quotes.push({quote, source});
-    } else if (citation === undefined) {
+    } else if (!citation) {
         quotes.push({quote, source, year});
-    } else if (year === undefined) {
+    } else if (!year) {
         quotes.push({quote, source, citation});
     } else {
         quotes.push({quote, source, citation, year});
     }
 }
 
-addQuote('let\'s play league', 'r4', 'discord', '2020');
-addQuote('bread wars', 'sp00ki', 'discord');
-addQuote('yeet boiiii', 'anon',);
-addQuote('rickyyy', 'scoot', undefined, '2020');
+addQuote('You miss 100% of the shots you don’t take', 'Wayne Gretzky', 'forbes.com', '2013');
+addQuote('You know you\'re in love when you can\'t fall asleep because reality is finally better than your dreams', 'Dr. Seuss', 'goodreads.com');
+addQuote('Learn JavaScript and you\'ll be fine', 'r4',);
+addQuote('What do you have to lose? ', 'Anonymous', '', '2020');
 
 /***
  * `getRandomQuote` function
-***/
+ ***/
 
-
+function getRandomQuote(quotes) {
+    let selectedQuote = quotes[Math.floor(Math.random() * (quotes.length))]
+    return selectedQuote
+}
 
 /***
  * `printQuote` function
-***/
+ ***/
+function printQuote(selectedQuote) {
+    let randomQuote = getRandomQuote(quotes);
+    let quoteSource = `<p class="quotes">${randomQuote.quote}</p><p class="source">${randomQuote.source}`;
+    if (randomQuote.citation) {
+        quoteSource += `<span class="citation">${randomQuote.citation}</span>`;
+    }
+    if (randomQuote.year) {
+        quoteSource += `<span class="year">${randomQuote.year}</span>`;
+    }
+    quoteSource += '</p>';
+    document.getElementById('quote-box').innerHTML = quoteSource;
+}
 
-
-
-/***
- * click event listener for the print quote button
- * DO NOT CHANGE THE CODE BELOW!!
-***/
+printQuote();
 
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
