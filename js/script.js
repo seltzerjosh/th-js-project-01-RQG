46ee3634a1ab6a574249f812ce071f7b1469ce89
Josh Seltzer <jselt88@gmail.com>
2020-09-27 15:47:22 -0400

    comments and redundancy clean

--- js/script.js
+++ js/script.js
@@ -3,15 +3,12 @@
  project 1 - A Random Quote Generator
  ******************************************/
 
-// For assistance:
-// Check the "Project Resources" section of the project instructions
-// Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
-
 /***
  * `quotes` array
  ***/
-const quotes = [];
+const quotes = []; //This line setsup quotes as an empty array
 
+//Function that checks for truthy quote and source, and optional citation and year before pushing new quote to array
 function addQuote(quote, source, citation, year) {
     if (!quote || !source) {
         console.log(`${quote} ${source} 'Needs a quote and source.'`);
@@ -25,26 +22,38 @@ function addQuote(quote, source, citation, year) {
         quotes.push({quote, source, citation, year});
     }
 }
-
+//Adding quotes
 addQuote('You miss 100% of the shots you don’t take', 'Wayne Gretzky', 'forbes.com', '2013');
 addQuote('You know you\'re in love when you can\'t fall asleep because reality is finally better than your dreams', 'Dr. Seuss', 'goodreads.com');
 addQuote('Learn JavaScript and you\'ll be fine', 'r4',);
 addQuote('What do you have to lose? ', 'Anonymous', '', '2020');
+addQuote('Just keep at it every day.', 'Anonymous', '', '2020');
 
 /***
  * `getRandomQuote` function
  ***/
-
+//Function that gets a quote from the array of quotes based on a random selection of quotes available
 function getRandomQuote(quotes) {
-    let selectedQuote = quotes[Math.floor(Math.random() * (quotes.length))]
-    return selectedQuote
+    return quotes[Math.floor(Math.random() * (quotes.length))]
 }
 
 /***
  * `printQuote` function
  ***/
-function printQuote(selectedQuote) {
-    let randomQuote = getRandomQuote(quotes);
+
+//Populates the randomQuote outside of the function to allow for a do while statement within the printQuote function
+    //This allows for a redundancy check against show the same quote twice in a row
+let randomQuote = getRandomQuote(quotes);
+
+//printQuote function defines a newRandomQuote as a call to getRandomQuote, and redoes this until it is not the same as random Quote
+    //Once this do while is successful, newRandomQuote replaces randomQuote, and is no longer used until the next call.
+    // It then populates the proper HTML to quoteSource and returns the "new" randomQuote.
+function printQuote() {
+    let newRandomQuote = getRandomQuote(quotes);
+    do {
+        newRandomQuote = getRandomQuote(quotes);
+    } while (randomQuote === newRandomQuote);
+    randomQuote = newRandomQuote;
     let quoteSource = `<p class="quotes">${randomQuote.quote}</p><p class="source">${randomQuote.source}`;
     if (randomQuote.citation) {
         quoteSource += `<span class="citation">${randomQuote.citation}</span>`;
@@ -54,8 +63,10 @@ function printQuote(selectedQuote) {
     }
     quoteSource += '</p>';
     document.getElementById('quote-box').innerHTML = quoteSource;
+    return randomQuote
 }
 
+//This is the first call to printQuote, which starts off the cycle
 printQuote();
-
+//Activates the 'Show another quote' button
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
